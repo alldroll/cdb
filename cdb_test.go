c3fac8289c5d1d475a931611c1693699db157f8f
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-01-15 17:38:42 +0300

    refactor tests with testify

--- cdb_test.go
+++ cdb_test.go
@@ -7,163 +7,141 @@ import (
 	"strconv"
 	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestShouldReturnAllValues(t *testing.T) {
-	f, _ := ioutil.TempFile("", "test_*.cdb")
-	defer f.Close()
-	defer os.Remove(f.Name())
+type testCDBRecord struct {
+	key []byte
+	val []byte
+}
+type CDBTestSuite struct {
+	suite.Suite
+	cdbFile     *os.File
+	cdbHandle   *CDB
+	testRecords []testCDBRecord
+}
 
-	handle := NewCDB()
+func TestCDBTestSuite(t *testing.T) {
+	suite.Run(t, new(CDBTestSuite))
+}
 
-	cases := []struct {
-		key, value string
-	}{
-		{"key1", "value1"},
-		{"key2", "value2"},
-		{"key3", "value3"},
-		{"key4", "value4"},
-		{"key5", "value5"},
-		{"key6", "value6"},
-		{"key7", "value7"},
-	}
-
-	writer, err := handle.GetWriter(f)
+func (suite *CDBTestSuite) getCDBReader() Reader {
+	// initialize reader
+	reader, err := suite.cdbHandle.GetReader(suite.cdbFile)
 	if err != nil {
-		t.Error(err)
+		suite.T().Errorf("Can't get CDB reader: %#v", err)
+		suite.T().FailNow()
+	}
+	return reader
+}
+
+func (suite *CDBTestSuite) getCDBWriter() Writer {
+	// initialize writer
+	writer, err := suite.cdbHandle.GetWriter(suite.cdbFile)
+	if err != nil {
+		suite.T().Errorf("Can't get CDB writer: %#v", err)
+		suite.T().FailNow()
+	}
+	return writer
+}
+
+func (suite *CDBTestSuite) SetupTest() {
+	f, err := ioutil.TempFile("", "test_*.cdb")
+	if err != nil {
+		suite.T().Errorf("Can't open temporary file: %#v", err)
+		suite.T().FailNow()
 	}
 
-	for _, c := range cases {
-		writer.Put([]byte(c.key), []byte(c.value))
+	suite.cdbFile = f
+	suite.cdbHandle = NewCDB()
+
+	// generate test records
+	const testRecordsCount = 10
+	testRecords := make([]testCDBRecord, testRecordsCount)
+	for i := 0; i < len(testRecords); i++ {
+		stri := strconv.Itoa(i)
+		testRecords[i].key = []byte("key" + stri)
+		testRecords[i].val = []byte("val" + stri)
 	}
+	suite.testRecords = testRecords
+}
 
-	writer.Close()
-	reader, _ := handle.GetReader(f)
+func (suite *CDBTestSuite) TearDownTest() {
+	err := suite.cdbFile.Close()
+	suite.Nil(err, "cant close cdb file")
+	err = os.Remove(suite.cdbFile.Name())
+	suite.Nil(err, "cant remove cdb file")
+}
 
-	for _, c := range cases {
-		value, err := reader.Get([]byte(c.key))
+func (suite *CDBTestSuite) fillTestCDB() {
+
+	writer := suite.getCDBWriter()
+	defer writer.Close()
+
+	for _, rec := range suite.testRecords {
+		err := writer.Put(rec.key, rec.val)
 		if err != nil {
-			t.Error(err)
-		}
-
-		if string(value) != c.value {
-			t.Errorf("Expected value %s, got %s", c.value, value)
+			suite.T().Error(err)
 		}
 	}
 
-	if reader.Size() != 7 {
-		t.Errorf("Expected size %d, got %d", 7, reader.Size())
+}
+
+func (suite *CDBTestSuite) TestWriteCDB() {
+	suite.fillTestCDB()
+}
+
+func (suite *CDBTestSuite) TestShouldReturnAllValues() {
+	suite.fillTestCDB()
+
+	reader := suite.getCDBReader()
+
+	for _, rec := range suite.testRecords {
+		value, err := reader.Get(rec.key)
+		suite.Nilf(err, "cant get from cdb key: %s", string(rec.key))
+
+		suite.Equalf(value, rec.val, "Test record value differs from the cdb's one")
+		exists, err := reader.Has(rec.key)
+		suite.Nilf(err, "error on check has key")
+		suite.True(exists)
+	}
+
+	suite.Equal(reader.Size(), len(suite.testRecords), "check records count")
+}
+
+func (suite *CDBTestSuite) TestShouldReturnNilOnNonExistingKeys() {
+	writer := suite.getCDBWriter()
+	writer.Close()
+
+	reader := suite.getCDBReader()
+
+	for _, rec := range suite.testRecords {
+		value, err := reader.Get(rec.key)
+		suite.Nilf(err, "cant get from cdb key: %s", string(rec.key))
+		suite.Nil(value, "Value must be nil for non existing keys")
+
+		exists, err := reader.Has(rec.key)
+		suite.Nilf(err, "error on check has key")
+		suite.False(exists)
 	}
 }
 
-func TestShouldReturnNilOnNonExistingKeys(t *testing.T) {
-	f, _ := os.Create("test.cdb")
-	defer f.Close()
-	defer os.Remove("test.cdb")
+func (suite *CDBTestSuite) TestConcurrentGet() {
+	suite.fillTestCDB()
 
-	handle := NewCDB()
+	reader := suite.getCDBReader()
 
-	cases := []struct {
-		key, value string
-	}{
-		{"key1", "value1"},
-		{"key2", "value2"},
-		{"key3", "value3"},
-		{"key4", "value4"},
-		{"key5", "value5"},
-		{"key6", "value6"},
-		{"key7", "value7"},
-	}
-
-	writer, err := handle.GetWriter(f)
-	if err != nil {
-		t.Error(err)
-	}
-
-	for _, c := range cases {
-		writer.Put([]byte(c.key), []byte(c.value))
-	}
-
-	writer.Close()
-	reader, _ := handle.GetReader(f)
-
-	keys := [...]string{
-		"nkey1",
-		"nkey2",
-		"nkey3",
-		"nkey4",
-		"nkey5",
-		"nkey6",
-		"nkey7",
-	}
-
-	for _, key := range keys {
-		value, err := reader.Get([]byte(key))
-		if err != nil {
-			t.Error(err)
-		}
-
-		if value != nil {
-			t.Errorf("Expected nil, got %s", value)
-		}
-
-		exists, err := reader.Has([]byte(key))
-		if err != nil {
-			t.Error(err)
-		}
-
-		if exists == true {
-			t.Error("Expected false, got true")
-		}
-	}
-}
-
-func TestConcurrentGet(t *testing.T) {
-	f, _ := os.Create("test.cdb")
-	defer f.Close()
-	defer os.Remove("test.cdb")
-
-	handle := NewCDB()
-
-	cases := []struct {
-		key, value string
-	}{
-		{"key1", "value1"},
-		{"key2", "value2"},
-		{"key3", "value3"},
-		{"key4", "value4"},
-		{"key5", "value5"},
-		{"key6", "value6"},
-		{"key7", "value7"},
-	}
-
-	writer, err := handle.GetWriter(f)
-	if err != nil {
-		t.Error(err)
-	}
-
-	for _, c := range cases {
-		writer.Put([]byte(c.key), []byte(c.value))
-	}
-
-	writer.Close()
-	reader, _ := handle.GetReader(f)
-
-	wg := sync.WaitGroup{}
+	wg := &sync.WaitGroup{}
 	for i := 0; i < 20; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
 
-			for _, c := range cases {
-				value, err := reader.Get([]byte(c.key))
-				if err != nil {
-					t.Error(err)
-				}
-
-				if string(value) != c.value {
-					t.Errorf("Expected value %s, got %s", c.value, value)
-				}
+			for _, rec := range suite.testRecords {
+				value, err := reader.Get(rec.key)
+				suite.Nilf(err, "Error while getting key")
+				suite.Equal(value, rec.val)
 			}
 		}()
 	}
@@ -171,48 +149,9 @@ func TestConcurrentGet(t *testing.T) {
 	wg.Wait()
 }
 
-func TestSetHash(t *testing.T) {
-	f, _ := os.Create("test.cdb")
-	defer f.Close()
-	defer os.Remove("test.cdb")
-
-	handle := NewCDB()
-	handle.SetHash(fnv.New32)
-
-	cases := []struct {
-		key, value string
-	}{
-		{"key1", "value1"},
-		{"key2", "value2"},
-		{"key3", "value3"},
-		{"key4", "value4"},
-		{"key5", "value5"},
-		{"key6", "value6"},
-		{"key7", "value7"},
-	}
-
-	writer, err := handle.GetWriter(f)
-	if err != nil {
-		t.Error(err)
-	}
-
-	for _, c := range cases {
-		writer.Put([]byte(c.key), []byte(c.value))
-	}
-
-	writer.Close()
-	reader, _ := handle.GetReader(f)
-
-	for _, c := range cases {
-		value, err := reader.Get([]byte(c.key))
-		if err != nil {
-			t.Error(err)
-		}
-
-		if string(value) != c.value {
-			t.Errorf("Expected value %s, got %s", c.value, value)
-		}
-	}
+func (suite *CDBTestSuite) TestSetHash() {
+	suite.cdbHandle.SetHash(fnv.New32)
+	suite.TestShouldReturnAllValues()
 }
 
 func BenchmarkGetReader(b *testing.B) {
